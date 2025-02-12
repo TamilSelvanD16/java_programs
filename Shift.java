0ff58a2230479d230598144f58caf111642163c4
TamilSelvanD16 <tamilgowtham777@gmail.com>
2025-02-12 14:17:14 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Shift
+{
+	public static void main(String[] args) {
+	   int a=10;
+	   a=a<<2;
+	   System.out.println("Left Shift:" + a);
+	   int b=10;
+	   b=b>>2;
+	   System.out.println("Right Shift:" +b);
+	  
+	}
+}
