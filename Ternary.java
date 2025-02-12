a45d6e37ab44a4b84be82a0c1aa76e4276916aa6
TamilSelvanD16 <tamilgowtham777@gmail.com>
2025-02-12 12:24:02 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+class Ternary
+{
+    public static void main(String args[])
+    {
+    int a,b,c;
+    a=12;
+    b=20;
+    c=17;
+    int max;
+    max=(a>b)?a:b;
+    System.out.println("Max using Ternary Operator:" + max);
+    }
+}
