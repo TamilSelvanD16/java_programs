13c853a72e9076d80bfb48a1c3391ea9cffb2c09
TamilSelvanD16 <tamilgowtham777@gmail.com>
2025-02-12 16:05:38 +0530

    Add files via upload

---
+++ mcq.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class mcq {
+    public static void main(String args[]) {
+         int a = 86;
+         int b = 94;
+         if(true)
+         {
+             System.out.println("a"+"b");
+         }
+         else{
+             System.out.println(a+b);
+         }
+    System.out.println("a"+b);
+    }
+}
